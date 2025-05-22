1db5ca8df570cc85f4a6a351714797e6093b404d
milev051 <milev051@gmail.com>
2025-05-22 16:28:01 +0200

    Nav bar

--- iswib-app/src/app/app.component.ts
+++ iswib-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, BottomNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
