4f7654b515633bb8ae9524043627fb9da759dddd
sotorg <it@worldyouthwave.org>
2025-06-22 19:38:25 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,4 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage'; // Rute
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "iswib-app-f9b80", appId: "1:36879671005:web:b3a42b809d49fe6d95c534", storageBucket: "iswib-app-f9b80.firebasestorage.app", apiKey: "AIzaSyB8c--YA_dStGomyUghQ7l9hx1E36LTV8c", authDomain: "iswib-app-f9b80.firebaseapp.com", messagingSenderId: "36879671005", measurementId: "G-E0KJH7S2HP" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
-  ]
-}).catch(err => console.error(err));
+
